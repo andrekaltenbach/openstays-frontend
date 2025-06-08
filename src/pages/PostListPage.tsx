9b833023326d0c2ec309779ecc0ceeb3c71e3bb5
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-06-08 13:11:14 +0200

    feat: enhance PostListPage layout

--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type{ Post } from "../types/index"; // Adjust the import path as necessary
 import axios from "axios";
+import { ToiletPaperIcon, WifiHighIcon, WifiSlashIcon, CookingPotIcon, WashingMachineIcon, BathtubIcon } from "@phosphor-icons/react";
+
 
 export default function PostListPage() {
   const [posts, setPosts] = useState<Post[] | null>(null);
@@ -25,12 +27,28 @@ export default function PostListPage() {
       { posts ? (
         posts.map((post) => (
           <div key={post.id} className="card">
+            <div className="flex flex-col sm:flex-row gap-6">
+            <img src={post.imageUrl} alt={post.title} className="w-full sm:w-48 h-48 object-cover mb-2 bg-amber-300" />
+            <div className="flex flex-col gap-2">
             <h2 className="text-xl font-semibold">{post.title}</h2>
-            <p className="text-gray-600">{post.location}</p>
-            <p className="text-gray-500">From: {post.fromDate} Until: {post.untilDate}</p>
+            <p className="text-gray-600 pb-3">{post.location}</p>
+            { post.permanentOffer ? (
+              <p className="text-green-500">Permanent Offer</p>) : (
+                post.fromDate && post.untilDate &&
+                <p className="text-gray-500">From: {post.fromDate} Until: {post.untilDate}</p>
+              )
+            }
+              <p className="text-gray-500">Max Nights: {post.maxNumberOfNights}</p>
+            <div className="flex gap-2">
+              <p className="text-gray-500">{post.hasFacilities && <ToiletPaperIcon size={24} weight="duotone"/>}</p>
+              <p className="text-gray-500">{post.hasWifi ? <WifiHighIcon size={24} weight="light" /> : <WifiSlashIcon size={24} weight="light" />}</p>
+              <p className="text-gray-500">{post.hasKitchen && <CookingPotIcon size={24} weight="duotone" />}</p>
+              <p className="text-gray-500">{post.hasWashingMachine && <WashingMachineIcon size={24} weight="duotone" />}</p>
+              <p className="text-gray-500">{post.hasShower && <BathtubIcon size={24} weight="duotone" />}</p>
+            </div>
             <p className="text-gray-500">Rating: {post.rating} ({post.ratingCount} reviews)</p>
-            <img src={post.imageUrl} alt={post.title} className="w-full h-48 object-cover mb-2" />
-            <p>{post.text}</p>
+          </div>
+          </div>
             <p className="mt-2 text-sm text-gray-400">Posted by {post.userName} on {new Date(post.createdAt).toLocaleDateString()}</p>
           </div>
         ))
